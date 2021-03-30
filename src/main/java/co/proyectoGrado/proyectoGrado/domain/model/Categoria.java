167b6b9b85c5ef71ea5b7c2496ca2c7f2b550076
Alejandro Jaramillo Giraldo <alejo88yi@gmail.com>
2021-03-29 19:51:24 -0500

    version 1

---
+++ src/main/java/co/proyectoGrado/proyectoGrado/domain/model/Categoria.java
@@ -0,0 +1,10 @@
+package co.proyectoGrado.proyectoGrado.domain.model;
+
+import java.util.List;
+
+public class Categoria {
+
+    private int idCategoria;
+    private List<Pregunta> pregunta;
+
+}
---
+++ src/main/java/co/proyectoGrado/proyectoGrado/domain/model/Curso.java
@@ -0,0 +1,9 @@
+package co.proyectoGrado.proyectoGrado.domain.model;
+
+public class Curso {
+    private int idCursos;
+    private String grado;
+    private String nombre;
+
+
+}
